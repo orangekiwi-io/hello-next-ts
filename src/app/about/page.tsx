580efb12549e01daa1a1cd294cadd3e6423f9ab8
Richard Locke <hello@orangekiwi.io>
2025-07-24 11:04:31 +0100

    Initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+	return (
+		<main>
+			<h1>About Page</h1>
+			<nav>
+				<a href="/">Home</a> | <a href="/contact">Contact</a>
+			</nav>
+		</main>
+	);
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function ContactPage() {
+	return (
+		<main>
+			<h1>Contact Page</h1>
+			<nav>
+				<a href="/">Home</a> | <a href="/about">About</a>
+			</nav>
+		</main>
+	);
+}
